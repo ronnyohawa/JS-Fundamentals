e104a66893291a6c9377c8fe8d09f13e82da8a71
RONNY OHAWA <78534612+ronnyohawa@users.noreply.github.com>
2025-09-25 16:54:39 +0300

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,8 +1,9 @@
-Write a script that prints a square
+const x = Number(process.argv[2]);
 
-    The first argument is the size of the square
-    If the first argument can’t be converted to an integer, print “Missing size”
-    You must use the character X to print the square
-    You must use console.log(...) to print all output
-    You are not allowed to use var
-    You must use a loop (while, for, etc.)
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
+}
